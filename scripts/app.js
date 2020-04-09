e7faa9b6637da7776205e9df7c69e204a0167cd2
Benga <benga.olasebikan@gmail.com>
2020-04-09 20:28:55 +0100

    board-button-on

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,49 @@
 function init() {
+  // DOM ELEMENTS
+  const grid = document.querySelector('.grid')
+  const startBtn = document.querySelector('#start')
+  const teamBtns = document.querySelectorAll('.team-button')
+  const cells = []
 
+  console.log(teamBtns)
+
+  // GRADE VARIABLES
+  const width = 7
+  const cellCount = width * width
+
+
+  //FUNCTIONS
+  function createCells() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      grid.appendChild(cell)
+      cell.textContent = i
+      cells.push(cell)
+    }
+  }
+
+  function gameStart() {
+    console.log('start game')
+    startBtn.classList.add('hidden')
+  }
+
+
+  function teamSelect(){
+    const arsenal = document.querySelector('#gooner')
+    console.log(arsenal)
+    const tottenham = document.querySelector('#spurs')
+    console.log(tottenham)
+  }
+
+
+  createCells()
+
+  // EVENT LISTENER
+  startBtn.addEventListener('click', gameStart)
+  teamBtns.forEach(btn => {
+    btn.addEventListener('click', teamSelect)
+  })
+  
 }
 
 window.addEventListener('DOMContentLoaded', init)

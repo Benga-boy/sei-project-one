92cfc83cb0500650916e02643da5a6c1e80f38eb
Benga <benga.olasebikan@gmail.com>
2020-04-14 21:20:22 +0100

    new-code

--- scripts/app.js
+++ scripts/app.js
@@ -2,51 +2,72 @@ function init() {
   // DOM ELEMENTS
   const grids = document.querySelector('.grid')
   const startBtn = document.querySelector('#start')
-  const teamBtns = document.querySelectorAll('.team-button')
-  const cells = []
-  let fields = document.querySelectorAll('.grid div')
   const curPlayer = document.querySelector('#current-player')
-  const player1Score = document.querySelector('#player1-score')
-  const player2Score = document.querySelector('#player2-score')
-
-  // GRID VARIABLES
-  const width = 7
-  const cellCount = width * 6
+  let fields = document.querySelectorAll('.grid div')
 
   // PLAYER CHOICE VARIABLES
-  // * Allows player to pick which team they want to be. Choice will be added fields class
-  let player1
-  let player2
-  
-
-
-  //FUNCTIONS
-  // * Creates the playing grids - 6 by 7
-  function createCells() {
-    for (let i = 0; i < cellCount; i++) {
-      const cell = document.createElement('div')
-      grids.appendChild(cell)
-      cell.textContent = i
-      cells.push(cell)
+  // * Assigns team values to player 1 and player 2
+  class Player {
+    constructor(team) {
+      this.team = team
+      this.plays = 0
     }
   }
 
-  // * Function for checking if a div has already been taking
+  const player1 = new Player('arsenal')
+  const player2 = new Player('spurs')
+
+  let currentPlayer
+
+  if (curPlayer.textContent === 'arsenal') {
+    currentPlayer = 'arsenal'
+  } else {
+    currentPlayer = 'tottenham'
+  }
+
+
+  // FUNCTIONS
+  class Grid {
+    constructor(rows, itemsInRows) {
+      this.grid = new Array(rows)
+
+      for (let i = 0; i < this.grid.length; i++){
+        this.grid[i] = new Array(itemsInRows)
+      }
+      this.grid.forEach(row => {
+        for (let i = 0; i < row.length; i++){
+          row[i] = document.createElement('div')
+          row[i].textContent = i + 'a'
+          grids.appendChild(row[i])
+        } 
+      })
+
+    }
+    addValue(x, y, player){
+      if (this.grid[x]  && this.grid[x][y]){
+        console.log('The slot is already occupied')
+        return
+      } else if (x > 0 && this.grid[x - 1][y] === undefined) {
+        console.log('The slot below isnt empty')
+        return
+      } else {
+        this.grid[x][y].classList.add(currentPlayer)
+        player.plays++
+      }
+    }
+  }
+
+  const gridCreator = new Grid(6, 7)
+
+  //* Function and array of winning combinations to check if there is a winner
+
 
   // * Function to start the games
   function gameStart() {
-    console.log('start game')
     startBtn.classList.add('hidden')
   }
 
   // * Team selection function
-  function teamSelect(event){
-    if (event.target.value === 'arsenal'){
-      player1 = 'arsenal'
-    } else {
-      player2 = 'tottenham'
-    }
-  }
 
   //Function when hovering over the grids
   function hoverBoard(event) {
@@ -60,21 +81,17 @@ function init() {
   // Function to add team logo to board
 
   function onCellClick(event) {
-    fields = event.target.classList.add(player1 || player2)  // - added from player choice variable depending on team selection
+    fields = event.target.classList.add(currentPlayer) 
+    console.log(fields.indexOf())
+    // - added from player choice variable depending on team selection
   }
 
   // EVENT LISTENERS
   startBtn.addEventListener('click', gameStart) // - starts the game 
   
-  teamBtns.forEach(btn => {
-    btn.addEventListener('click', teamSelect) // event listener for team selection
-  })
-
   grids.addEventListener('mouseover',  hoverBoard)
   grids.addEventListener('mouseout', hoverOut)
   grids.addEventListener('click', onCellClick)
-
-  createCells()
   
 }
 
